3c7d8184813a69f71aecbbc97c988f3237a776f4
gvgh <sanchirastogi02@gmail.com>
2019-09-16 05:56:21 +0530

    add

--- app/src/main/java/com/example/fireapp/MainActivity.java
+++ app/src/main/java/com/example/fireapp/MainActivity.java
@@ -4,13 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static int SPLASH_TIME_OUT =3000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         startActivity(new Intent(MainActivity.this,pollution_weather.class));
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, Sign_up_and_Sign_in.class);
+                startActivity(intent);
+                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
+
     }
 }
