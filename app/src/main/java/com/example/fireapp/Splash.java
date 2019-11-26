880a41a353b65a4b13ce785eea5819c730787d2d
sanchi0204 <sanchirastogi02@gmail.com>
2019-11-26 20:27:09 +0530

    earth added

---
+++ app/src/main/java/com/example/fireapp/Splash.java
@@ -0,0 +1,26 @@
+package com.example.fireapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Splash extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT =3000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splash.this,Home.class);
+                startActivity(intent);
+                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
+    }
+}
