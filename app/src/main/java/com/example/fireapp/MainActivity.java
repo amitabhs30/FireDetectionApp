3e886dab3495fec5269d9d954099e3ea9960aa37
sanchi0204 <sanchirastogi02@gmail.com>
2019-11-27 11:54:36 +0530

    spaslh

--- app/src/main/java/com/example/fireapp/MainActivity.java
+++ app/src/main/java/com/example/fireapp/MainActivity.java
@@ -13,38 +13,23 @@ import android.widget.Button;
 import static com.example.fireapp.R.id.fragment;
 
 public class MainActivity extends AppCompatActivity {
-    private Button b;
-//    private static int SPLASH_TIME_OUT =3000;
+
+    private static int SPLASH_TIME_OUT =3000;
 //  View fragment = findViewById(R.id.fragment);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        b= findViewById(R.id.btn);
-        b.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, Home.class);
-                startActivity(intent);
-            }
-        });
-//        fragment.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Intent intent = new Intent(MainActivity.this,Home.class);
-//                startActivity(intent);
-//            }
 
-//        new Handler().postDelayed(new Runnable() {
-//            @Override
-//            public void run() {
-//                Intent intent = new Intent(MainActivity.this,Home.class);
-//                startActivity(intent);
-//                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
-//                finish();
-//            }
-//        },SPLASH_TIME_OUT);
-//        });
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this,Home.class);
+                startActivity(intent);
+                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
     }
 }
